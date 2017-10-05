de4aecd5241b8acd0655e68defc6485a5be25e98
Quinn Gates <gates111@mail.chapman.edu>
2017-10-05 18:18:30 +0000

    added things

--- test_array_calculus.py
+++ test_array_calculus.py
@@ -6,7 +6,7 @@ def test_derivative():
     """Tests derivative(a,b,n) with following tests:
         - derivative(0,9,10) multiplied by f = [0,1,2,3,4,5,6,7,8,9]
     """
-    actual = [1,1,1,1,1,1,1,1,1,1]
-    trial = ac.derivative(0,9,10)@[0,1,2,3,4,5,6,7,8,9]
+    actual = np.array([1,1,1,1,1,1,1,1,1,1])
+    trial = ac.derivative(0,9,10)@(np.array([0,1,2,3,4,5,6,7,8,9]))
     print("Testing: actual ?= trial: ",actual," += ",trial)
     np.testing.assert_almost_equal(actual,trial)
