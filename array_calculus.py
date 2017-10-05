b2ed61eb6e819f3d424c4bba85d9f8aaab89fc04
Quinn Gates <gates111@mail.chapman.edu>
2017-10-05 21:23:41 +0000

    Fixed itgit status!

--- array_calculus.py
+++ array_calculus.py
@@ -3,5 +3,12 @@
 import numpy as np
 def derivative(a,b,n):
     deltax = (b-a)/(n-1)
-    D = (np.eye(n,n,1)-np.eye(n,n,-1))/(deltax*2)  #(np.eye(n)*(-2)+
+    D = (np.eye(n,n,1)-np.eye(n,n,-1)) #(np.eye(n)*(-2)+
+    D[0][0] = -1
+    D[-1][-1] = 1
+    dub = np.eye(n)
+    dub[0][0] = 2
+    dub[-1][-1] = 2
+    print(dub)
+    D = (dub@D)/(deltax*2)
     return D
